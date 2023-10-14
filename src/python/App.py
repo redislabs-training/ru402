89ae44f0b8038aed3c541d18c2f857396fb711af
Mirko Ortensi <mortensi@mirko.ortensi-TN97MGGHPH>
2023-10-14 17:40:52 +0200

    RedisVL example done

---
+++ src/python/App.py
@@ -0,0 +1,34 @@
+from redisvl.index import SearchIndex
+from redisvl.query import VectorQuery
+from redisvl.vectorize.text import HFTextVectorizer
+
+
+# initialize the index and connect to Redis
+index = SearchIndex.from_yaml("schema.yaml")
+index.connect("redis://localhost:6379")
+
+# initialize the embedder
+hf = HFTextVectorizer(model="sentence-transformers/all-MiniLM-L6-v2")
+
+# create the index in Redis
+index.create(overwrite=True)
+
+# load data into the index in Redis (list of dicts)
+data = [
+    {'content': 'That is a very happy person', 'genre': 'persons', 'embedding': hf.embed('That is a very happy person', as_buffer=True)},
+    {'content': 'That is a happy dog', 'genre': 'pets', 'embedding': hf.embed('That is a happy dog', as_buffer=True)},
+    {'content': 'Today is a sunny day', 'genre': 'weather', 'embedding': hf.embed('Today is a sunny day', as_buffer=True)}
+]
+
+index.load(data)
+
+# perform the VSS query
+query = VectorQuery(
+    vector=hf.embed('That is a happy person'),
+    vector_field_name="embedding",
+    return_fields=["content"],
+    num_results=3,
+)
+
+results = index.query(query)
+print(results)
---
+++ src/python/README.md
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+pip install redisvl
+pip install sentence_transformers
+
+
+python App.py
+
+
+[{'id': 'doc:e993b504b5254f31b809f3ba8c65e31d', 'vector_distance': '0.114169858396', 'content': 'That is a very happy person'}, {'id': 'doc:2f6714bf5f3643b6b73b0c7f8f146b48', 'vector_distance': '0.610845565796', 'content': 'That is a happy dog'}, {'id': 'doc:c993c84f60c04c6d91908c09dfc3036d', 'vector_distance': '1.48624742031', 'content': 'Today is a sunny day'}]
