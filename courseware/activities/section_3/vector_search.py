113fd3242c27f0d0f035abdb5a97d58e1796cf66
Mirko Ortensi <mortensi@mirko.ortensi-TN97MGGHPH>
2023-11-27 22:36:31 +0100

    Improve vector_search.py for Redis connectivity

--- courseware/activities/section_3/vector_search.py
+++ courseware/activities/section_3/vector_search.py
@@ -1,15 +1,26 @@
 import redis
+from redis.commands.search.field import TextField, TagField, VectorField
+from redis.commands.search.indexDefinition import IndexDefinition
 from redis.commands.search.query import Query
 import numpy as np
-from numpy.linalg import norm
 from sentence_transformers import SentenceTransformer
 
 
 # Get a Redis connection
-r = redis.Redis(host='localhost', port=6379)
+r = redis.Redis(host='localhost', 
+                port=6379,
+                username="default",
+                password="secret")
 
 # Define the model we want to use 
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+
+# Create the index
+index_def = IndexDefinition(prefix=["doc:"])
+schema = (  TextField("content", as_name="content"),
+            TagField("genre", as_name="genre"),
+            VectorField("embedding", "HNSW", {"TYPE": "FLOAT32", "DIM": 384, "DISTANCE_METRIC": "COSINE"}))
+r.ft('doc_idx').create_index(schema, definition=index_def)
 
 # Import sample data
 r.hset('doc:1', mapping = {'embedding': model.encode("That is a very happy person").astype(np.float32).tobytes(), 'genre': 'persons', 'content': "That is a very happy person"})
